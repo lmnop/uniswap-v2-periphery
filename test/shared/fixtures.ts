fad2519ec30041db73034f6ddc158fdaf9dcecf0
Eric Olszewski <eolszewski@gmail.com>
2020-10-16 14:23:37 +0300

    Add router v3 to testing suite + fixtures.

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -14,6 +14,7 @@ import UniswapV1Factory from '../../build/UniswapV1Factory.json'
 import UniswapV2Router01 from '../../build/UniswapV2Router01.json'
 import UniswapV2Migrator from '../../build/UniswapV2Migrator.json'
 import UniswapV2Router02 from '../../build/UniswapV2Router02.json'
+import UniswapV3Router01 from '../../build/UniswapV3Router01.json'
 import RouterEventEmitter from '../../build/RouterEventEmitter.json'
 
 const overrides = {
@@ -29,6 +30,7 @@ interface V2Fixture {
   factoryV2: Contract
   router01: Contract
   router02: Contract
+  router03: Contract
   routerEventEmitter: Contract
   router: Contract
   migrator: Contract
@@ -57,6 +59,7 @@ export async function v2Fixture(provider: Web3Provider, wallets: Wallet[]): Prom
   // deploy routers
   const router01 = await deployContract(wallet, UniswapV2Router01, [factoryV2.address, WETH.address], overrides)
   const router02 = await deployContract(wallet, UniswapV2Router02, [factoryV2.address, WETH.address], overrides)
+  const router03 = await deployContract(wallet, UniswapV3Router01, [factoryV2.address, WETH.address], overrides)
 
   // event emitter for testing
   const routerEventEmitter = await deployContract(wallet, RouterEventEmitter, [])
@@ -93,7 +96,8 @@ export async function v2Fixture(provider: Web3Provider, wallets: Wallet[]): Prom
     factoryV2,
     router01,
     router02,
-    router: router02, // the default router, 01 had a minor bug
+    router03,
+    router: router03, // the default router, 01 had a minor bug
     routerEventEmitter,
     migrator,
     WETHExchangeV1,
